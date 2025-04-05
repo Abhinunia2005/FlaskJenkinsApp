7f82a8bba1fdc270cfa3e9fb396a5333ffe6444b
Abhinunia2005 <91672008+Abhinunia2005@users.noreply.github.com>
2025-04-05 09:39:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from flask import Flask
-from flasgger import Swagger
 
 app = Flask(__name__)
-swagger = Swagger(app)
 
-@app.route('/')
+@app.route("/")
 def home():
-    """
-    Home Endpoint
-    ---
-    get:
-      description: Welcome to Flask Swagger!
-      responses:
-        200:
-          description: Returns a welcome message
-    """
-    return "Hello, Swagger in Flask!"
+    return "Hello, Azure from Jenkins!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
